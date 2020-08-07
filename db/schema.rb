eeb5527985c3baa8b654d8760ac44c5166d51f2c
naohiro-178 <69035007+naohiro-178@users.noreply.github.com>
2020-08-07 11:04:11 +0900

    roomsテーブル作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_05_100135) do
+ActiveRecord::Schema.define(version: 2020_08_07_015742) do
+
+  create_table "rooms", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "name", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
